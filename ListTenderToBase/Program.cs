6e4eb1727c93e9389c2e46ffdbb938e901133ec1
Дениска Зеленцов <zelentsov36@gmail.com>
2023-02-22 01:50:29 +0300

    Data transfer between modules and adding

--- ListTenderToBase/Program.cs
+++ ListTenderToBase/Program.cs
@@ -9,11 +9,11 @@ namespace ListTenderToBase
         public string? Method { get; set; }
         public string? Act { get; set; }
         public int PlatformId { get; set; }
-        public DateTime? DeadlineStard { get; set; }
+        public DateTime? DeadlineStart { get; set; }
         public DateTime? DeadlineEnd { get; set; }
-        public double? InitialPrice { get; set; }
-        public string? Organization { get; set; }
-        public string? OrganizationAdress { get; set; }
+        public int TimeZoneId { get; set; }
+        public decimal? InitialPrice { get; set; }
+        public int OrganizationId { get; set; }
         public string? ProcurementObject { get; set; }
         public string? PlaceOfDelivery { get; set; }
         public string? SupplyAssurance { get; set; }
@@ -34,15 +34,19 @@ namespace ListTenderToBase
     {
         static void Main(string[] args)
         {
+            ListSource.ListSource source = new ListSource.ListSource();
+
+
             using (ApplicationContext db = new ApplicationContext())
             {
-                db.Database.EnsureDeleted();
-                db.Database.EnsureCreated();
-                // создаем два объекта User
-                Procurement user1 = new Procurement { DeadlineEnd = Convert.ToDateTime("24.10.2003 18:34"), PlatformId = 1 };
+                Procurement procurement = new Procurement
+                {
+                    Number = source.listProcurement[0],
+                    DeadlineEnd = Convert.ToDateTime("24.10.2003 18:34"), PlatformId = 1, InitialPrice = Convert.ToDecimal(34535.34)
+                };
 
                 // добавляем их в бд
-                db.Procurements.AddRange(user1);
+                db.Procurements.AddRange(procurement);
                 db.SaveChanges();
             }
             // получение данных
@@ -53,7 +57,7 @@ namespace ListTenderToBase
                 Console.WriteLine("Users list:");
                 foreach (Procurement u in users)
                 {
-                    Console.WriteLine($"{u.ID}.{u.DeadlineEnd:g} - {u.PlatformId}");
+                    Console.WriteLine($"{u.ID}.{u.DeadlineEnd:g} - {u.PlatformId} - {u.Number}");
                 }
             }
         }
